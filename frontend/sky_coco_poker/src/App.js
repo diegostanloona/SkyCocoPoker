b634d281908b8950e5f44661b588b7b70cf23a97
Diego Alejandro Gomez <diegogomezweb@gmail.com>
2021-09-27 11:42:39 -0500

    added routes

--- frontend/sky_coco_poker/src/App.js
+++ frontend/sky_coco_poker/src/App.js
@@ -1,12 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+// import Users from './user/pages/Users';
+// import NewPlace from './places/pages/NewPlace';
+// import UserPlaces from './places/pages/UserPlaces';
+// import UpdatePlace from './places/pages/UpdatePlace';
+// import Auth from './user/pages/Auth';
+import LoadingSpinner from './shared/components/UIElements/LoadingSpinner';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
+import { useAuth } from './shared/hooks/auth-hook';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import MainGame from './game/pages/MainGame';
+
+const App = () => {
+
+    const {token, login, logout, userId} = useAuth();
+
+    let routes;
+
+    if (true) {
+        routes = (
+            <Switch>
+              <Route path="/" exact>
+              //home
+              </Route>
+              <Route path="/game" exact>
+                <MainGame/>
+              </Route>
+             <Route path="/:userId" exact>
+             //profile
+              </Route>
+              <Redirect to="/"/>
+            </Switch>
+        );
+    } else {
+        routes = (
+            <Switch>
+              <Route path="/" exact>
+              </Route>
+              <Route path="/auth">
+              </Route>
+              <Redirect to="/auth"/>
+            </Switch>
+        );
+    }
+
+    return (
+        <AuthContext.Provider value={{isLoggedIn: !token, token: token, userId:userId, login: login, logout: logout}}>
+          <Router>
+          <MainNavigation/>
+            <main>
+              <Suspense fallback={<div className="center"><LoadingSpinner></LoadingSpinner></div>}>
+                {routes}
+              </Suspense>
+            </main>
+          </Router>
+        </AuthContext.Provider>
+    );
 }
 
 export default App;
--- frontend/sky_coco_poker/src/shared/components/Navigation/NavLinks.js
+++ frontend/sky_coco_poker/src/shared/components/Navigation/NavLinks.js
@@ -6,19 +6,19 @@ import './NavLinks.css';
 
 const NavLinks = props => {
 	const auth = useContext(AuthContext);
- 
+
 	return ( <ul className="nav-links">
 		<li>
-			<NavLink to="/" exact>ALL USERS</NavLink>
+			<NavLink to="/" exact>HOME</NavLink>
 		</li>
 		{auth.isLoggedIn && (
 		<li>
-			<NavLink to={`/${auth.userId}/places`}>MY PLACES</NavLink>
+			<NavLink to="/game">GAME</NavLink>
 		</li>
 		)}
 		{auth.isLoggedIn && (
 		<li>
-			<NavLink to="/places/new">ADD PLACE</NavLink>
+			<NavLink to={`/${auth.userId}`}>MY PROFILE</NavLink>
 		</li>
 		)}
 		{!auth.isLoggedIn && (
